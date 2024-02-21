ea6c2b138cb87bca816f382592ffb5d81d4d2771
Devika Makan <108333894+devika2711@users.noreply.github.com>
2024-02-21 19:21:56 +0000

    variables

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId= 2711 // const is used to declare constant objects or variables. cannot be reassigned.
+let accountEmail= "abc@gmail.com"
+accountEmail= "dm@dm.com" // let can be reassigned.
+
+/*
+prefer not to use var becs of issue in block scope and functional scope.
+*/
+
+console.table([accountId, accountEmail])
