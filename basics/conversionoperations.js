183a167c2aee9be1a466cc0c8c2638c8e87f195f
Devika Makan <108333894+devika2711@users.noreply.github.com>
2024-02-21 19:20:12 +0000

    conversion of datatypes

---
+++ basics/conversionoperations.js
@@ -0,0 +1,25 @@
+let score= 33
+let score2= "33"
+console.log(typeof(score))
+console.log(typeof score2)
+
+let value= Number(score2)
+console.log(typeof value)
+console.log(value)
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1; false => 0
+
+let isLoggedIn= 1
+let boolean= Boolean(isLoggedIn)
+console.log(boolean)
+
+// 1=>true; 0=>false;
+// "" = false
+// "devika"= true
+
+let number= 27
+let stringno= String(number)
+console.log(stringno)
+console.log(typeof stringno)
