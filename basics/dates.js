f6d59c7b41576fae94be590d8a220b6c22dd27c7
Devika Makan <108333894+devika2711@users.noreply.github.com>
2024-02-22 16:04:36 +0000

    dates

---
+++ basics/dates.js
@@ -0,0 +1,10 @@
+let mydate= new Date()
+/*console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toLocaleString())
+console.log(mydate.toJSON())
+console.log(mydate.toISOString())
+console.log(typeof mydate) */
+
+let timestamp= Date.now()
+console.log(timestamp)
